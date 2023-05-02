8d351e9b0686af9076b9bc6e23c3d8c0414d5da8
Nikki18977 <54531227+Nikki18977@users.noreply.github.com>
2023-05-02 16:17:36 +0300

    Update 02_data.sql

--- docker-entrypoint-initdb.d/02_data.sql
+++ docker-entrypoint-initdb.d/02_data.sql
@@ -1,7 +1,7 @@
 -- just emulate hard work
-SELECT clock_timestamp(), pg_sleep(3), clock_timestamp();
+SELECT clock_timestamp(), pg_sleep(30), clock_timestamp();
 
-INSERT INTO "users"("login", "password", "roles")
+INSERT INTO "users"("login", "password", "roles")https://github.com/Nikki18977/0401_db/blob/main/docker-entrypoint-initdb.d/02_data.sql
 VALUES
 ('root', '{argon2}$argon2id$v=19$m=4096,t=3,p=1$VD2gbE9s9SvxSU3QnmeO9w$hosiwDgCWLdyZ6xrysnDg9fDE38frM65jxOj58ZkCXs', '{ROOT}'),
 ('admin', '{argon2}$argon2id$v=19$m=4096,t=3,p=1$VD2gbE9s9SvxSU3QnmeO9w$hosiwDgCWLdyZ6xrysnDg9fDE38frM65jxOj58ZkCXs', '{ADMIN}'),
